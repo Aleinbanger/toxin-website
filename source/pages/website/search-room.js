68c65f29a1547a1139295e7e8882a119c594ce7f
Aleinbanger <58362714+Aleinbanger@users.noreply.github.com>
2020-05-08 23:45:26 +0300

    Finish Search Room page
    Add responsiveness to search-room__filter.
    Plus minor fixes for PerfectPixel.

--- source/pages/website/search-room.js
+++ source/pages/website/search-room.js
@@ -5,11 +5,41 @@ class SearchRoom {
     this.block = block;
     this.blockName = this.block.classList[0];
     this._initialize();
+    this._bindEventListeners();
   }
 
   _initialize() {
+    this.filterBtn = this.block.querySelector(`.js-${this.blockName}__filter-button`);
+    this.filter = this.block.querySelector(`.js-${this.blockName}__filter`);
+    this.state = {
+      filterActive: false,
+    };
+    this._renderResults();
+    this._renderState();
+  }
+
+  _renderResults() {
     const results = this.block.querySelector(`.js-${this.blockName}__results`);
     this.results = new SearchRoomResults(results);
+  }
+
+  _bindEventListeners() {
+    this.filterBtn.addEventListener('click', () => this._handleFilterBtnClick());
+  }
+
+  _handleFilterBtnClick() {
+    this.state.filterActive = !this.state.filterActive;
+    this._renderState();
+  }
+
+  _renderState() {
+    if (this.state.filterActive) {
+      this.filter.classList.add(`${this.blockName}__filter_active`);
+      this.filterBtn.classList.add(`${this.blockName}__filter-button_active`);
+    } else {
+      this.filter.classList.remove(`${this.blockName}__filter_active`);
+      this.filterBtn.classList.remove(`${this.blockName}__filter-button_active`);
+    }
   }
 }
 
