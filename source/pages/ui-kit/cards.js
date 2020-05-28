ddb40797cd07ed70abc21948d9addff8ed63e563
Aleinbanger <58362714+Aleinbanger@users.noreply.github.com>
2020-05-28 23:25:06 +0300

    Fix and refactor ui-kit pages

--- source/pages/ui-kit/cards.js
+++ source/pages/ui-kit/cards.js
@@ -1,1 +1,24 @@
+/* eslint-disable import/no-unresolved */
 import './cards.scss';
+
+import CardRoom from 'blocks/card-room/card-room';
+
+class Cards {
+  constructor(block) {
+    this.blockName = 'cards';
+    this.block = block;
+    this._initialize();
+  }
+
+  _initialize() {
+    this.block.querySelectorAll(`.js-${this.blockName}__card-room`)
+      .forEach((cardRoom) => new CardRoom(cardRoom));
+  }
+}
+
+function renderBlocks() {
+  const blocks = document.querySelectorAll('.js-cards');
+  blocks.forEach((block) => new Cards(block));
+}
+
+export default renderBlocks();
--- source/pages/ui-kit/form-elements.js
+++ source/pages/ui-kit/form-elements.js
@@ -1,1 +1,40 @@
+/* eslint-disable import/no-unresolved */
 import './form-elements.scss';
+
+import ButtonLike from 'blocks/button-like/button-like';
+import Rating from 'blocks/rating/rating';
+import Comment from 'blocks/comment/comment';
+import Pagination from 'blocks/pagination/pagination';
+import DropdownDate from 'blocks/dropdown-date/dropdown-date';
+
+class FormElements {
+  constructor(block) {
+    this.blockName = 'form-elements';
+    this.block = block;
+    this._initialize();
+  }
+
+  _initialize() {
+    this.block.querySelectorAll(`.js-${this.blockName}__like-button`)
+      .forEach((buttonLike) => new ButtonLike(buttonLike));
+
+    this.block.querySelectorAll(`.js-${this.blockName}__rate-button`)
+      .forEach((rating) => new Rating(rating));
+
+    this.block.querySelectorAll(`.js-${this.blockName}__comment`)
+      .forEach((comment) => new Comment(comment));
+
+    this.block.querySelectorAll(`.js-${this.blockName}__pagination`)
+      .forEach((pagination) => new Pagination(pagination));
+
+    this.block.querySelectorAll(`.js-${this.blockName}__dropdown-date`)
+      .forEach((dropdownDate) => new DropdownDate(dropdownDate));
+  }
+}
+
+function renderBlocks() {
+  const blocks = document.querySelectorAll('.js-form-elements');
+  blocks.forEach((block) => new FormElements(block));
+}
+
+export default renderBlocks();
